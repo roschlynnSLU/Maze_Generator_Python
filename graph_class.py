542ef4453866648ae0b7b09ecabb2344aaaa2648
roschlynnSLU <127329528+roschlynnSLU@users.noreply.github.com>
2023-03-31 06:45:42 +0000

    Added class files

---
+++ graph_class.py
@@ -0,0 +1,7 @@
+import random
+
+class Graph:
+
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.graph = {}
--- maze_generator_python.py
+++ maze_generator_python.py
@@ -1,15 +1,17 @@
-class Graph:
-
-    def __init__(self, num_nodes):
-        self.num_nodes = num_nodes
-        self.graph = {}
+import random
+import graph_class
         
 class Maze:
-    
+
     def __init__(self, size):
         self.size = size
         self.nodes = []
-        self.graph = Graph(size*size)
+        self.graph = graph_class.Graph(size*size)
+
+    def get_total_nodes(self):
+        return max(self.graph) + 1
+
+    def print_Maze_using_DFS(maze, start_node):
 
 maze = Maze(5)
 print("Welcome to 2D maze")
