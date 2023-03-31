453eac7a51e43e3f8d7eabe581fb548b5db4ab00
roschlynnSLU <127329528+roschlynnSLU@users.noreply.github.com>
2023-03-31 06:04:47 +0000

    Changes in Structure

--- maze_generator_python.py
+++ maze_generator_python.py
@@ -1,19 +1,15 @@
-#Python Code to Randomly Generate a Maze
-
 class Graph:
-    def __init__(self, num_Nodes):
-        self.num_Nodes = num_Nodes
-        self.graph = {}
 
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.graph = {}
+        
 class Maze:
     
     def __init__(self, size):
         self.size = size
         self.nodes = []
-        self.graph = Graph(size * size)
-    
-    def print_test():
-        print("Welcome to 2D Maze Generator")
+        self.graph = Graph(size*size)
 
 maze = Maze(5)
-maze.print_test()
+print("Welcome to 2D maze")
